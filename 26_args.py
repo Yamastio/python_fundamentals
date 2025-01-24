a1adf2c5f3befc26bfd88eb214ad70c7e7461ae9
Yamastio <mikuchan312314@gmail.com>
2025-01-25 05:24:32 +0700

    Add: 26_args.py

---
+++ 26_args.py
@@ -0,0 +1,15 @@
+# args = Argument Opsional
+# menerima data tuple
+# args tidak disimpan dalam dictionary, tapi berkerja layaknya tuple yang kosong yang siap diisi
+# jika ingin parameter berupa deret nilai maka gunakan args
+# jika ingin parameter berupa key, value maka gunakan kwargs
+
+
+def fungsi_kita(*args) -> None:
+
+    # print dari Argument opsional
+    for arg in args:
+        print(arg)
+
+
+fungsi_kita(7, 11, 12, 16, 20)
