9f95f2b4ff8696c0404d31bf2754579c9d5286bb
Yamastio <mikuchan312314@gmail.com>
2025-01-26 04:25:07 +0700

    Enhance: 26_args more detailed explanation

--- 26_args.py
+++ 26_args.py
@@ -1,4 +1,5 @@
-# args = Argument Opsional
+# args = Argument Positional
+# args akan menangkap semua argument posisi yang diberikan dan menyimpan dalam bentuk tuple
 # menerima data tuple
 # args tidak disimpan dalam dictionary, tapi berkerja layaknya tuple yang kosong yang siap diisi
 # jika ingin parameter berupa deret nilai maka gunakan args
